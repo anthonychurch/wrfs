2b35c33583773de0ff478ffdf1369acf8abfc38c
Anthony Church <aw_church@yahoo.com.au>
2024-04-16 20:40:34 +1000

    Delete java_wrfs_responder_check/src directory

--- java_wrfs_responder_check/src/GetCurrentTimeStamp.java
+++
@@ -1,9 +0,0 @@
- 
-import java.sql.Timestamp;
- 
-public class GetCurrentTimeStamp{
-	public Timestamp get(){
-		java.util.Date date= new java.util.Date();
-		return new Timestamp(date.getTime());
-	} 
-}
